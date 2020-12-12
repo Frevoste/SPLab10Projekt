f0de71e0feb6c1814a7688a557eaa255487104fd
Mateusz <m.m.redzimski@gmail.com>
2020-12-12 20:32:19 +0100

    Edycja pliku main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double pierwsza,druga;
+cout << "Podaj pierwsza liczbe";
+cin >> pierwsza;
+cout << "Podaj druga liczbe";
+cin >> druga;
+cout << "Co chcesz zrobic? \n1.Dodac \n2.Odjac \n3.Mnozyc \n4.Dzielic";
+int menu;
+cin >> menu;
+switch (menu)
+{
+case 1:
+	cout << "1";
+case 2:
+	cout << "2";
+case 3:
+	cout << "3";
+case 4:
+	cout << "4";
+}
+
+
+
+return 0;
+}
